d9c2d3787227ec93c378994f2485daf9be535044
Crysflair <Mysrio@outlook.com>
2019-01-12 11:14:06 +0800

    Add building blocks

---
+++ Arithmetic/Arithmetic/Expression.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arithmetic
+{
+    // abstruct class. 
+    // Subclassed: VariableReference & Operation
+    public abstract class Expression
+    {
+        public abstract Number Evaluate(Dictionary<string, object> vars);
+    }
+
+    public class VariableReference : Expression
+    {
+        private string name;
+        public VariableReference(string name)
+        {
+            this.name = name;
+        }
+        public override Number Evaluate(Dictionary<string, object> vars)
+        {
+            object value = vars[name];
+            if (value == null)
+            {
+                throw new Exception("Unknown variable name: " + name);
+            }
+            return (Number)value;
+        }
+    }
+    public class Operation : Expression
+    {
+        private Expression left;
+        private char op;
+        private Expression right;
+
+        public Operation(Expression left, char op, Expression right)
+        {
+            this.left = left;
+            this.op = op;
+            this.right = right;
+        }
+        public override Number Evaluate(Dictionary<string, object> vars)
+        {
+            Number x = left.Evaluate(vars);
+            Number y = right.Evaluate(vars);
+            switch (op)
+            {
+                case '+': return x.Add(y);
+                case '-': return x.Sub(y);
+                case '*': return x.Mul(y);
+                case '/': return x.Div(y);  // TODO: divide zero?
+                case '^': return x.Pow(y);  
+            }
+            throw new Exception("Unknown operator");
+        }
+    }
+}
---
+++ Arithmetic/Arithmetic/Number.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arithmetic
+{
+    public class Number
+    {
+        private readonly int Numerator;
+        private readonly int Denominator;
+
+        public Number(int numerator, int denominator)
+        {
+            Numerator = numerator;
+            Denominator = denominator;
+        }
+
+        public Number Add(Number a, Number b)
+        {
+
+        }
+
+        public Number Sub(Number a, Number b)
+        {
+
+        }
+
+        public Number Mul(Number a, Number b)
+        {
+
+        }
+
+        public Number Div(Number a, Number b)
+        {
+            // TODO: Maybe we should check that b is not zero
+        }
+
+        public Number Pow(Number a, Number b)
+        {
+            // TODO: Maybe Number b must be a integar
+        }
+
+    }
+}
