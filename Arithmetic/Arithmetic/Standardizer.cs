7d8a85cbf332674af9d80d033179edf046c6c820
Crysflair <Mysrio@outlook.com>
2019-01-18 11:45:33 +0800

    done: Standardizer. Tested.

--- Arithmetic/Arithmetic/Standardizer.cs
+++ Arithmetic/Arithmetic/Standardizer.cs
@@ -16,16 +16,20 @@ namespace Arithmetic
         static Regex neg_frac = new Regex(@"^-(?<num1>\d*)/(?<num2>\d*)$", RegexOptions.Compiled | RegexOptions.IgnoreCase);
         static Regex posi0_frac = new Regex(@"^(?<num1>\d*)/(?<num2>\d*)$", RegexOptions.Compiled | RegexOptions.IgnoreCase);
 
-        static string deci_2_frac(string left_string, string right_string, bool is_neg)
+
+        static Number neg_one_node = new Number(-1, 1);
+        static Number pos_one_node = new Number(1, 1);
+
+        // facility
+        public static string deci_standardize(string left_string, string right_string, bool is_neg)
         {
             Number left = new Number(int.Parse(left_string), 1);
             Number right = new Number(int.Parse(right_string), Number.IntPow(10, right_string.Length));
-
             Number if_neg_node;
             if (is_neg)
-                if_neg_node = new Number(-1, 1);
+                if_neg_node = neg_one_node;
             else
-                if_neg_node = new Number(1, 1);
+                if_neg_node = pos_one_node;
 
             // construct expression
             Operation result_abs = new Operation(
@@ -43,12 +47,13 @@ namespace Arithmetic
             return result.Evaluate(variable_to_number).ToString();
         }
 
-        string Standardize_Number(string input)
+        // CORE CODE
+        public static string Standardize_Number(string input)
         {
             //// description:
             //
             //1. accepted format rule
-            //  - space can exist everywhere
+            //  - space and comma can exist everywhere
             //  - no parenthesis !
             //  - one negative sign or no sign, at the beginning of the string.
             //  - fractions unreduced are supported
@@ -65,8 +70,11 @@ namespace Arithmetic
             //  "<integar> <.> <integar>"
             //  "<integar> "
             ////
-            // Remove Space
+            
+
+            
             input = input.Replace(" ", "");
+            input = input.Replace(",", "");
             MatchCollection matches = null;
 
             // 分数
@@ -74,18 +82,28 @@ namespace Arithmetic
             if (matches.Count == 1) // Hit!
             {
                 var match = matches[0];
-                int num1 = int.Parse(match.Groups["num1"].Value);
-                int num2 = int.Parse(match.Groups["num2"].Value);
-                return new Number(-num1, num2).ToString();
+                try //Try Parse
+                {
+                    int num1 = int.Parse(match.Groups["num1"].Value);
+                    int num2 = int.Parse(match.Groups["num2"].Value);
+                    return new Number(-num1, num2).ToString();
+                }
+                catch (Exception) {; }
+                
             }
 
             matches = posi0_frac.Matches(input);
             if (matches.Count == 1) // Hit!
             {
                 var match = matches[0];
-                int num1 = int.Parse(match.Groups["num1"].Value);
-                int num2 = int.Parse(match.Groups["num2"].Value);
-                return new Number(num1, num2).ToString();
+                try
+                {
+                    int num1 = int.Parse(match.Groups["num1"].Value);
+                    int num2 = int.Parse(match.Groups["num2"].Value);
+                    return new Number(num1, num2).ToString();
+                }
+                catch (Exception) {; }
+
             }
 
             // 整数
@@ -93,16 +111,26 @@ namespace Arithmetic
             if (matches.Count == 1) // Hit!
             {
                 var match = matches[0];
-                int num1 = int.Parse(match.Groups["num"].Value);
-                return new Number(-num1, 1).ToString();
+                try
+                {
+                    int num1 = int.Parse(match.Groups["num"].Value);
+                    return new Number(-num1, 1).ToString();
+                }
+                catch (Exception) {; }
+
             }
 
             matches = posi0_int.Matches(input);
             if (matches.Count == 1) // Hit!
             {
                 var match = matches[0];
-                int num1 = int.Parse(match.Groups["num"].Value);
-                return new Number(num1, 1).ToString();
+                try
+                {
+                    int num1 = int.Parse(match.Groups["num"].Value);
+                    return new Number(num1, 1).ToString();
+                }
+                catch (Exception) {; }
+
             }
 
             // 小数
@@ -110,17 +138,26 @@ namespace Arithmetic
             if (matches.Count == 1) // Hit!
             {
                 var match = matches[0];
-                string left_string = match.Groups["num1"].Value;
-                string right_string = match.Groups["num2"].Value;
-                return deci_2_frac(left_string, right_string, is_neg: true);
+                try
+                {
+                    string left_string = match.Groups["num1"].Value;
+                    string right_string = match.Groups["num2"].Value;
+                    return deci_standardize(left_string, right_string, is_neg: true);
+                }
+                catch (Exception) {; }
+
             }
             matches = posi0_digi.Matches(input);
             if (matches.Count == 1) // Hit!
             {
                 var match = matches[0];
-                string left_string = match.Groups["num1"].Value;
-                string right_string = match.Groups["num2"].Value;
-                return deci_2_frac(left_string, right_string, is_neg: false);
+                try
+                {
+                    string left_string = match.Groups["num1"].Value;
+                    string right_string = match.Groups["num2"].Value;
+                    return deci_standardize(left_string, right_string, is_neg: false);
+                }
+                catch (Exception) {; }
             }
 
             return "ERROR";
---
+++ Arithmetic/ArithmeticTests/StandardizerTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Arithmetic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arithmetic.Tests
+{
+    [TestClass()]
+    public class StandardizerTests
+    {
+        [TestMethod()]
+        public void deci_2_fracTest()
+        {
+            Assert.AreEqual(Standardizer.deci_standardize("3", "5", true), "(-7/2)");
+            Assert.AreEqual(Standardizer.deci_standardize("003", "500", false), "(7/2)");
+            Assert.AreEqual(Standardizer.deci_standardize("101", "0000", false), "101");
+            Assert.AreEqual(Standardizer.deci_standardize("01", "0000", true), "(-1)");
+        }
+
+        [TestMethod()]
+        public void Standardize_NumberTest()
+        {
+            Assert.AreEqual(Standardizer.Standardize_Number("- 4 4 / 2"),"(-22)");
+            Assert.AreEqual(Standardizer.Standardize_Number("- 1 0 . 2"), "(-51/5)");
+            Assert.AreEqual(Standardizer.Standardize_Number("- 4"), "(-4)");
+            Assert.AreEqual(Standardizer.Standardize_Number("4 / 3"), "(4/3)");
+            Assert.AreEqual(Standardizer.Standardize_Number("6.25"), "(25/4)");
+            Assert.AreEqual(Standardizer.Standardize_Number(" 666  "), "666");
+            Assert.AreEqual(Standardizer.Standardize_Number("1,591.000,000,000"), "1591");
+
+
+            Assert.AreEqual(Standardizer.Standardize_Number("+59"), "ERROR");
+            Assert.AreEqual(Standardizer.Standardize_Number("77.5/3"), "ERROR");
+            Assert.AreEqual(Standardizer.Standardize_Number("(-597)"), "ERROR");
+            Assert.AreEqual(Standardizer.Standardize_Number("-.981"), "ERROR");
+            Assert.AreEqual(Standardizer.Standardize_Number("982."), "ERROR");
+            
+        }
+    }
+}
